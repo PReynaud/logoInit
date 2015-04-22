ef3a3642671a2564c31ed7cea1c37bb185a67b29
PReynaud <pierre.reynaud@hotmail.fr>
2015-04-22 17:44:02 +0200

    Models are completed

---
+++ src/logoInit/model/Segment.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.awt.*;
+
+/**
+ * Created by Pierre on 22/04/2015.
+ */
+public class Segment {
+    public Point ptStart, ptEnd;
+    public Color color;
+
+    public Segment() {
+        ptStart = new Point(0,0);
+        ptEnd = new Point(0,0);
+    }
+}
--- src/logoInit/model/TurtleList.java
+++ src/logoInit/model/TurtleList.java
@@ -1,11 +1,26 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by Pierre on 22/04/2015.
  */
 public class TurtleList {
-    private ArrayList<Turtle> turtles;
+    private ArrayList<Turtle> tortues; // la liste des tortues enregistrees
 
+    public TurtleList() {
+        tortues = new ArrayList<Turtle>();
+    }
+
+    public void addTortue(Turtle o) {
+        tortues.add(o);
+    }
+
+    public void reset() {
+        for (Iterator it = tortues.iterator();it.hasNext();) {
+            Turtle t = (Turtle) it.next();
+            t.reset();
+        }
+    }
 }
