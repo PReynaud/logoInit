98aa8d0ec9dca74ec903e5e2516f27df5afde4f6
PReynaud <pierre.reynaud@hotmail.fr>
2015-04-27 17:51:34 +0200

    Refactor de la vue des tortues

--- src/logoInit/view/GraphPanel.java
+++ src/logoInit/view/GraphPanel.java
@@ -31,69 +31,23 @@ public class GraphPanel extends JPanel {
         }
     }
 
-    private TurtleList tortues; // la liste des tortues enregistrees
     private Graphics graph;
+    private TurtleView turtleView;
 
     public GraphPanel(TurtleList turtles, Dimension dimension) {
         super();
-        tortues = turtles;
+        this.turtleView = new TurtleView(turtles);
         this.setBackground(Color.white);
         this.setSize(dimension);
         this.setPreferredSize(dimension);
-        this.drawTurtle(graph, turtles.getCurrentTurtle());
-    }
-
-    public void addTortue(Turtle o) {
-        tortues.addTortue(o);
+        this.turtleView.drawTurtle(graph, turtles.getCurrentTurtle());
     }
 
     public void reset() {
-       tortues.reset();
+       this.turtleView.reset();
     }
 
-    public void showTurtles(Graphics g) {
-        for(Iterator it = tortues.getTurtles().iterator(); it.hasNext();) {
-            Turtle t = (Turtle) it.next();
-            drawTurtle(g, t);
-        }
-    }
 
-    public void drawTurtle (Graphics graph, Turtle turtle) {
-        if (graph==null)
-            return;
-
-        //Calcule les 3 coins du triangle a partir de
-        // la position de la tortue p
-        Point p = new Point(turtle.getX(),turtle.getY());
-        Polygon arrow = new Polygon();
-
-        //Calcule des deux bases
-        //Angle de la droite
-        double theta=Turtle.RATIO_DEG_RAD*(-turtle.getDir());
-        //Demi angle au sommet du triangle
-        double alpha=Math.atan( (float)Turtle.RB / (float)Turtle.RP );
-        //Rayon de la fleche
-        double r=Math.sqrt( Turtle.RP*Turtle.RP + Turtle.RB *Turtle.RB);
-        //Sens de la fleche
-
-        //Pointe
-        Point p2=new Point((int) Math.round(p.x+r*Math.cos(theta)),
-                (int) Math.round(p.y-r*Math.sin(theta)));
-        arrow.addPoint(p2.x,p2.y);
-        arrow.addPoint((int) Math.round( p2.x-r*Math.cos(theta + alpha) ),
-                (int) Math.round( p2.y+r*Math.sin(theta + alpha) ));
-
-        //Base2
-        arrow.addPoint((int) Math.round(p2.x - r * Math.cos(theta - alpha)),
-                (int) Math.round(p2.y + r * Math.sin(theta - alpha)));
-
-        arrow.addPoint(p2.x, p2.y);
-        graph.setColor(turtle.decodeColor(turtle.getColor()));
-        graph.fillPolygon(arrow);
-        if(turtle instanceof ImprovedTurtle){
-            graph.drawString(((ImprovedTurtle) turtle).getName(), turtle.getX() + 20, turtle.getY());
-        }
-    }
 
     @Override
     public void paintComponent(Graphics g) {
@@ -106,7 +60,7 @@ public class GraphPanel extends JPanel {
         g.fillRect(0,0,dim.width, dim.height);
         g.setColor(c);
 
-        showTurtles(g);
+        this.turtleView.showTurtles(g);
     }
 
 
